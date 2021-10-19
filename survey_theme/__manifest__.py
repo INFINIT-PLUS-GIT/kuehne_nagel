dcff9e0e9694b3fa7d4d04a73d2c248071f3ab46
cguachan <87678691+cguachan@users.noreply.github.com>
2021-10-19 14:34:26 -0500

    add filter company

--- survey_theme/__manifest__.py
+++ survey_theme/__manifest__.py
@@ -19,7 +19,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['survey'],
+    'depends': ['survey','contacts'],
 
     # always loaded
     'data': [
