dde63e71293e0fdf09cd86a99be803581481f5fc
cguachan <87678691+cguachan@users.noreply.github.com>
2021-10-25 08:09:08 -0500

    add send mail to laggardds

--- survey_theme/models/survey_invite.py
+++ survey_theme/models/survey_invite.py
@@ -19,9 +19,44 @@ class SurveyInvite(models.TransientModel):
         string='Add by tags',
         comodel_name='res.partner.category',
     )
+    exist_laggards = fields.Boolean(string="Send to laggards")
+    show_laggards = fields.Boolean(string='Number of laggards',
+        compute='_compute_laggards' )
     
+    @api.depends('survey_id')
+    def _compute_laggards(self):
+        for record in self:
+            user_inputs_ids = record.survey_id.user_input_ids.filtered(lambda r: r.state == 'new' and r.test_entry == False).mapped("partner_id.id")
+            if len(user_inputs_ids):
+                record.show_laggards = True
+            else:
+                record.show_laggards = False
+    @api.onchange('survey_id')
+    def _onchange_survey_id(self):
+        self.template_id = self.env.ref('survey_theme.mail_template_user_input_invite_KN').id
+
+    @api.onchange('exist_laggards')
+    def load_user_incomplete(self):
+        for r in self:
+            contacts_ids = r.env['res.partner'].search([('category_id','in',r.category_ids.ids),('email','!=',''),('parent_id','!=',False)])
+            if self.exist_laggards:
+                laggards = self.env['res.partner'].browse(self.survey_id.user_input_ids.filtered(lambda r: r.state == 'new' and r.test_entry == False).mapped("partner_id.id"))
+                for laggard in laggards:
+                    if laggard not in contacts_ids:
+                        contacts_ids += laggard
+            r.write({'partner_ids':[(6, None, contacts_ids.ids)]})
+        
+        
+        
     @api.onchange('category_ids')
     def _onchange_category(self):
         for r in self:
-            r.write({'partner_ids':[(6, None, r.env['res.partner'].search([('category_id','in',r.category_ids.ids),('email','!=',''),('parent_id','!=',False)]).ids)]})
-            
+            contacts_ids = r.env['res.partner'].search([('category_id','in',r.category_ids.ids),('email','!=',''),('parent_id','!=',False)])
+            if self.exist_laggards:
+                laggards = self.env['res.partner'].browse(self.survey_id.user_input_ids.filtered(lambda r: r.state == 'new' and r.test_entry == False).mapped("partner_id.id"))
+                for laggard in laggards:
+                    if laggard not in contacts_ids:
+                        contacts_ids += laggard
+            r.write({'partner_ids':[(6, None, contacts_ids.ids)]})
+
+
